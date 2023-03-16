078e30f361c16e2a0bb40aa6c2185547ed318fcd
Hao Ran Lai <hrlai.ecology@gmail.com>
2023-03-17 11:16:49 +1300

    add trait.sigma

--- ci.R
+++ ci.R
@@ -12,19 +12,26 @@ mod <- readRDS("Lai_et_al_2020_JEcol_boral_output.rds")
 # of the parameters of interest
 # for this demonstration, we will focus on the fourth corner coefficients
 # which are named traits.coefs and traits.int in boral
+# we also need trait.sigma, which is the residual variance in species' environmental
+# responses not explained by traits (species random intercepts)
+# generating CI with trait.sigma is akin to getting prediction intervals
+# whereas without trait.sigma is akin to getting confidence intervals (to borrow
+# nomenclature from the frequentist world...)
+# anyhow, I suggest generating CI including trait.sigma so it is more conservative
 draws <- get.mcmcsamples(mod)
 
 # extract and tidy up the posterior
 # I'll call them kappa following Francis Hui's nomenclature
 kappa <- 
   draws %>% 
-  # gather the traits.coefs and traits.int into a long list
+  # gather the traits.coefs, traits.int and trait.sigma into a long list
   # the env and trait are names for the row and column indices
   # where row is env and column is trait
   # note that Fig. 1 in Lai et al. (2020) transposes this matrix so that
   # we present it as a "trait"-by-"env" table (sorry for the potential confusion)
   spread_draws(traits.coefs[env, trait],
-               traits.int[env])
+               traits.int[env],
+               trait.sigma[env])
 
 # note that traits.int is the main trait effects, which corresponds to
 # the top row in Fig. 1 of Lai et al. (2020)
@@ -70,7 +77,10 @@ WD_canopy <-
   # but the expected response to canopy due to WD, while holding all other traits
   # at their averages!
   # I'll call it partial slope for clarity
-  mutate(beta_canopy_partial = traits.int + traits.coefs * WD_new) %>% 
+  mutate(beta_canopy_partial = 
+           traits.int + 
+           traits.coefs * WD_new +
+           rnorm(n(), 0, trait.sigma)) %>% 
   # then we summarise it to median +- 95% CI
   group_by(WD_new) %>% 
   median_qi(beta_canopy_partial)
@@ -82,7 +92,9 @@ ggplot(WD_canopy) +
   geom_hline(yintercept = 0, lty = 2) +
   geom_ribbon(aes(WD_new, ymin = .lower, ymax = .upper),
               alpha = 0.2) +
-  geom_line(aes(WD_new, beta_canopy_partial))
+  geom_line(aes(WD_new, beta_canopy_partial)) +
+  labs(x = "WD (scaled)",
+       y = "Response to canopy opening")
 
 # As you can see, it is a bit different from the original Fig. 2a!
 # Apologies, this is because PhD Hao Ran was a noob back then and custom coded the
